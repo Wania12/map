caacc3ce31d26df659cdba8728e2997fdbf03605
Wania12 <63885320+Wania12@users.noreply.github.com>
2022-01-15 11:58:13 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+//import { GoogleApiWrapper } from 'google-maps-react';
+//(imp)import logo from './logo.svg';
+
+function App() {
+    return (
+
+        <div className="App">
+            <h1>Google Maps App</h1>
+            <GoogleMap />
+        </div>
+    );
+}
+
+
+
+
+//<div className="App">
+//    <header className="App-header">
+//        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+
+//        {/*<a*/}
+//        {/*    className="App-link"*/}
+//        {/*    href="https://reactjs.org"*/}
+//        {/*    target="_blank"*/}
+//        {/*    rel="noopener noreferrer"*/}
+//        {/*>*/}
+//        {/*    Wania application*/}
+//        {/*</a>*/}
+
+//    </header>
+
+//        </div>
+//    );
+//}
+
+export default App;
